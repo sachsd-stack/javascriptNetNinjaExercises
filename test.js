3e90b9620041a072589f0693dd624b9f4a46401f
sachsd <sachsd1116@outlook.com>
2020-10-12 00:45:49 -0400

    Not Sure if Done correctly

---
+++ test.js
@@ -0,0 +1,44 @@
+// var a = 7;
+// var b = 5;
+
+// console.log(a+b);
+// console.log(typeof (a+b));
+// console.log(Math.round(7.8));
+// console.log(Math.floor(7.8));
+// console.log(Math.ceil(7.8));
+// console.log(Math.max(7, 4, 9, 8));
+
+
+
+// function getAverage(a,b) {
+
+//     var average = (a + b)/2; //local variable
+//     console.log(average);
+//     return average;
+
+// }
+
+// var myResult = getAverage(7,12); //global variable
+
+// function logResult() {
+//     console.log("the average is " + myResult + " inside the function"); 
+// }
+
+// logResult();
+
+
+// var a = 6;
+// var b = 5;
+
+// // double negative
+// if (isNaN(a)) {
+//     console.log("that aint even a number, thickie");
+// } else {
+//     console.log("meaning of life is " + (a*b));
+// }
+
+
+var string1 = "Abc";
+var string2 = "Bcd";
+
+console.log(string1 < string2);
