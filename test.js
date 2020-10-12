d6ef8faac74e6278bde923353202062402a2e177
sachsd <sachsd1116@outlook.com>
2020-10-12 01:11:24 -0400

    Up to Exercise 30

--- test.js
+++ test.js
@@ -37,8 +37,60 @@
 //     console.log("meaning of life is " + (a*b));
 // }
 
-
+// From Exercise 24
 var string1 = "Abc";
 var string2 = "Bcd";
 
 console.log(string1 < string2);
+
+// var myArray = new Array();
+// myArray[0] = 8;
+// myArray[1] = "hello";
+
+// var myCar = new Object();
+// myCar.maxSpeed = 50;
+// myCar.driver = "Shaun";
+// myCar.drive = function() { console.log("now driving");};
+
+// myCar.drive();
+
+
+//From Exercise 29
+// var myCar = {
+//     maxSpeed: 70, 
+//     driver: "Net Ninja", 
+//     drive: function(speed, time) { 
+//         console.log(speed * time);
+//     }, 
+//     // test: function() {
+//     //     console.log(this);
+//     // }
+//     logDriver: function() {
+//         console.log("driver name is " + this.driver);
+//     }
+// };
+
+// myCar.logDriver();
+// // myCar2.test();
+// console.log(myCar.maxSpeed);
+// myCar.drive(50, 3);
+
+//Exercise 30
+var Car = function(maxSpeed, driver) {
+    this.maxSpeed = maxSpeed;
+    this.driver = driver;
+    this.drive = function(speed, time) { 
+        console.log(speed * time);
+    }; 
+    this.logDriver = function() {
+        console.log("driver name is " + this.driver);
+    };
+}
+
+var myCar = new Car(70, "Ninja Man");
+var myCar2 = new Car(40, "Humpty Dumpty");
+var myCar3 = new Car(10, "Shaun");
+var myCar4 = new Car(90, "James Bond");
+
+myCar.drive(30,5);
+myCar3.logDriver();
